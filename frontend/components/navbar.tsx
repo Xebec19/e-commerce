0ecff8f91b318382a88873b50aee5a1d0332276c
bihari123 <thakur.cs.tarun@gmail.com>
2023-09-13 22:51:25 +0530

    wip feat(navbar): adding navbar and updated favicon image

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { MdMenu } from "react-icons/md";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import React from "react";
+
 export default function Navbar() {
-  return <h1>Hello world!</h1>;
+  const [showSidebar, setShowSidebar] = React.useState<boolean>(false);
+
+  function toggleSidebar() {
+    setShowSidebar(!showSidebar);
+  }
+
+  return (
+    <nav className="py-4 flex justify-around items-center">
+      <MdMenu onClick={toggleSidebar} />
+      <span>E-commerce</span>
+      <AiOutlineShoppingCart />
+    </nav>
+  );
 }
