1e8c3f6639b433b835b0b70be9c024e3cd0558ff
xebec19 <rohant946@gmail.com>
2023-09-15 22:46:53 +0530

    feat(navbar): added sidebar for mobile header

--- frontend/components/layout/navbar/mobile-header.tsx
+++ frontend/components/layout/navbar/mobile-header.tsx
@@ -1,11 +1,13 @@
 import { SITE_NAME } from "@/lib/environment";
 import { Menu, ShoppingCart } from "lucide-react";
 import Image from "next/image";
+import { Sidebar } from "./sidebar";
+import { Button } from "@/components/ui/button";
 
 export default function MobileHeader() {
   return (
     <div className="flex justify-between p-4 items-center">
-      <Menu />
+      <Sidebar />
       <span className="flex items-center justify-center space-x-4">
         <Image
           priority
@@ -16,7 +18,9 @@ export default function MobileHeader() {
         />
         <span className="font-medium">E commerce</span>
       </span>
-      <ShoppingCart />
+      <Button variant="outline">
+        <ShoppingCart />
+      </Button>
     </div>
   );
 }
---
+++ frontend/components/layout/navbar/sidebar.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+export function Sidebar() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <p>Link1</p>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <p>Link2</p>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
