1e32f658db6a9d90025b8c9950c49e8b1bddebfb
xebec19 <rohant946@gmail.com>
2023-09-30 23:06:05 +0530

    feat(cart page): added cart page

--- frontend/components/lists/cart-list-edit.tsx
+++ frontend/components/lists/cart-list-edit.tsx
@@ -2,25 +2,25 @@
 
 import { Minus, Plus, X } from "lucide-react";
 import Image from "next/image";
-import CouponForm from "../forms/coupon-form";
-import { Button } from "../ui/button";
 
 function CartItem() {
   return (
-    <li className="flex flex-wrap justify-between items-center">
-      <div className="w-[80px] h-[80px]">
+    <li className="flex justify-between items-center space-x-4 py-4 px-2">
+      <div className="w-[80px] h-[80px] relative overflow-hidden rounded-md">
         <Image fill src={"/dummy-t-shirt.jpg"} alt="some awesome product" />
-        <X className="absolute right-0 top-0" />
+        <span className="rounded-full p-1 bg-black">
+          <X size={"20px"} />
+        </span>
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col flex-1">
         <span className="text-xl">Super awesome tshirt</span>
         <span className="text-md text-slate">White / M</span>
       </div>
 
       <div className="flex flex-col">
-        <span className="text-xl">$ 30.00 USD</span>
-        <div className="rounded-full border flex">
+        <span className="text-md text-right pr-1">$ 30.00 </span>
+        <div className="rounded-full border flex px-2 space-x-2 py-2">
           <Minus />
           <span>2</span>
           <Plus />
@@ -30,25 +30,15 @@ function CartItem() {
   );
 }
 
-// TODO add amount details here
-function Amount() {
-  return <div className="flex-col divide-y"></div>;
-}
-
 export default function CartListEdit() {
   return (
-    <>
-      <ul className="my-4 max-w-[768px] divide-y list-none">
+    <div className="max-w-[768px] mb-4">
+      <ul className="divide-y list-none border rounded-md">
         <CartItem />
         <CartItem />
         <CartItem />
         <CartItem />
       </ul>
-      <CouponForm />
-      <Amount />
-      <div>
-        <Button className="uppercase"> Proceed to Checkout</Button>
-      </div>
-    </>
+    </div>
   );
 }
