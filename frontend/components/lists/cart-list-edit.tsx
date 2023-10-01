3435eaf09fe99e410c02de0394811765b25e9e6e
xebec19 <rohant946@gmail.com>
2023-10-01 10:45:44 +0530

    feat(cart page): completed cart page ui

--- frontend/components/lists/cart-list-edit.tsx
+++ frontend/components/lists/cart-list-edit.tsx
@@ -2,23 +2,40 @@
 
 import { Minus, Plus, X } from "lucide-react";
 import Image from "next/image";
+import { Tooltip, TooltipProvider } from "../ui/tooltip";
+import { TooltipContent, TooltipTrigger } from "@radix-ui/react-tooltip";
+
+function RemoveFromCartIcon() {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <span className="absolute dark:text-black-500 right-0 top-0 cursor-pointer">
+            <X className="dark:text-black" />
+          </span>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Remove from cart</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
 
 function CartItem() {
   return (
-    <li className="flex justify-between items-center space-x-4 py-4 px-2">
+    <li className="flex justify-between items-start space-x-4 py-4 px-2">
       <div className="w-[80px] h-[80px] relative overflow-hidden rounded-md">
         <Image fill src={"/dummy-t-shirt.jpg"} alt="some awesome product" />
-        <span className="rounded-full p-1 bg-black">
-          <X size={"20px"} />
-        </span>
+        <RemoveFromCartIcon />
       </div>
 
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 space-y-2">
         <span className="text-xl">Super awesome tshirt</span>
         <span className="text-md text-slate">White / M</span>
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col space-y-2">
         <span className="text-md text-right pr-1">$ 30.00 </span>
         <div className="rounded-full border flex px-2 space-x-2 py-2">
           <Minus />
