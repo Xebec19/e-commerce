56d0d1b66be5fe58f23eb53d155ad2254e5250c6
xebec19 <rohant946@gmail.com>
2023-11-18 15:36:27 +0530

    feat: added login screen

--- admin-ui/src/main.tsx
+++ admin-ui/src/main.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import "./globals.css";
 import ErrorPage from "./error-page";
+import { ThemeProvider } from "./stores/theme.provider";
+import Root from "./routes/root";
+import Login from "./routes/auth/login.page";
+import AppProvider from "./stores/app.provider";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>App is working</div>,
+    element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "auth",
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </AppProvider>
   </React.StrictMode>
 );
