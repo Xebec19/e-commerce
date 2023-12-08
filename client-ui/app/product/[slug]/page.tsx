5c847c89399498432eb795c894706711ae34750b
xebec19 <rohant946@gmail.com>
2023-12-08 23:00:04 +0530

    WIP new arrivals in product/slug page

--- client-ui/app/product/[slug]/page.tsx
+++ client-ui/app/product/[slug]/page.tsx
@@ -96,6 +96,26 @@ export default async function ProductPage({
           ))}
         </section>
       </div>
+
+      <div className="py-4">
+        <h3 className="prose text-xl font-bold mb-2">New Arrivals</h3>
+        <section
+          role="similar products"
+          className="flex space-x-2 overflow-x-auto w-full"
+        >
+          {categoryResponse.payload.map((product, index) => (
+            <div
+              key={product.product_id}
+              className="aspect-square w-[80vw] md:w-[33vw] h-[400px] border"
+            >
+              <ProductCard
+                payload={product}
+                sizes="(min-width: 768px) 33vw, 80vw"
+              />
+            </div>
+          ))}
+        </section>
+      </div>
     </>
   );
 }
