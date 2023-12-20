e2e1cb9a958d2b0bd5a94fb2e14c51c8d91e06d9
xebec19 <rohant946@gmail.com>
2023-12-20 21:30:27 +0530

    wip: search page

--- client-ui/app/search/page.tsx
+++ client-ui/app/search/page.tsx
@@ -1,25 +1,50 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Search } from "lucide-react";
 import { useState } from "react";
-import SearchDialog from "@/components/search/search-dialog";
-import { Slider } from "@/components/ui/slider";
 
 export default function Page() {
-  const [query, setQuery] = useState<string>("");
-  const [error, setError] = useState<string>("");
+  const [priceOrder, setPriceOrder] = useState<"asc" | "desc">("asc");
 
   return (
     <article role="search">
       <div className="my-2 space-y-2">
         <h3 className="prose text-xl font-bold mb-2">Search</h3>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="bg-background h-full border p-2 md:px-4">
-            <div></div>
-            <Slider defaultValue={[50]} max={100} step={1} />
+        <div className="flex justify-between">
+          <div className="flex space-x-2">
+            <Input placeholder="Search products" type="search" />
+            <Button variant={"ghost"} size={"icon"}>
+              <Search />
+            </Button>
           </div>
 
-          <div className="md:col-span-2">Search pages</div>
+          <div className="flex space-x-2 justify-center items-center">
+            <Select>
+              <SelectTrigger id="price">
+                <SelectValue placeholder="Order by price" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="asc">Price: Low to High</SelectItem>
+                  <SelectItem value="desc">Price: High to Low</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4"></div>
       </div>
     </article>
   );
