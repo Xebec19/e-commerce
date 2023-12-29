2c9fa39c0c7983c7e597dd8d7204889dec5b8a12
xebec19 <rohandeveloper106@gmail.com>
2023-12-29 22:17:59 +0530

    wip: search filters

--- client-ui/app/search/page.tsx
+++ client-ui/app/search/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ProductCard from "@/components/product/product-card";
+import NoSearchFound from "@/components/search/no-search-found";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -12,18 +14,24 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { fetchCategories } from "@/lib/http/product.http";
+import { IProductPayload } from "@/interfaces/product.interface";
+import { querySearch, querySearchWithFilters } from "@/lib/algolia";
+import { fetchCategories, fetchNewProducts } from "@/lib/http/product.http";
+import { addSearch } from "@/store/search.slice";
 import { useQuery } from "@tanstack/react-query";
 import { Search } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 
 export default function Page() {
-  const [priceOrder, setPriceOrder] = useState<string>("asc");
+  const [query, setQuery] = useState<string>("");
+  const [priceOrder, setPriceOrder] = useState<string>("order asc");
   const [selectedCat, setSelectedCat] = useState<string[]>([]);
+  const [hits, setHits] = useState<IProductPayload[]>([]);
+
+  const dispatch = useDispatch();
   const searchParams = useSearchParams();
-  const categoriesParam = searchParams.getAll("category");
-  const order = searchParams.get("order");
   const pathname = usePathname();
   const router = useRouter();
 
@@ -42,6 +50,19 @@ export default function Page() {
     }
   }
 
+  async function handleSearch() {
+    let payload = {
+      query,
+      categories: selectedCat,
+      order: [priceOrder],
+    };
+
+    const results = await querySearchWithFilters(payload);
+    setHits(results);
+
+    dispatch({ type: addSearch, payload: query });
+  }
+
   useEffect(() => {
     let params = new URLSearchParams();
     params.append("order", priceOrder);
@@ -54,31 +75,37 @@ export default function Page() {
     <article role="search">
       <div className="my-2 space-y-2">
         <h3 className="prose text-xl font-bold mb-2">Search</h3>
-        <div className="flex flex-col md:flex-row space-y-2 justify-between">
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 justify-between items-center">
           <div className="flex space-x-2">
-            <Input placeholder="Search products" type="search" />
-            <Button variant={"ghost"} size={"icon"}>
+            <Input
+              placeholder="Search products"
+              type="search"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setQuery(e.target.value)
+              }
+            />
+            <Button variant={"ghost"} size={"icon"} onClick={handleSearch}>
               <Search />
             </Button>
           </div>
 
-          <div className="flex space-x-2 justify-start items-center">
-            <Select
-              value={priceOrder}
-              onValueChange={(value) => setPriceOrder(value)}
-            >
-              <SelectTrigger id="price" className="max-w-[11rem]">
-                <SelectValue placeholder="Order by price" />
-              </SelectTrigger>
+          {/* <div className="flex space-x-2 justify-start items-center"> */}
+          <Select
+            value={priceOrder}
+            onValueChange={(value) => setPriceOrder(value)}
+          >
+            <SelectTrigger id="price" className="max-w-[11rem]">
+              <SelectValue placeholder="Order by price" />
+            </SelectTrigger>
 
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="asc">Price: Low to High</SelectItem>
-                  <SelectItem value="desc">Price: High to Low</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="order asc">Price: Low to High</SelectItem>
+                <SelectItem value="order desc">Price: High to Low</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          {/* </div> */}
         </div>
 
         <div className="flex space-x-2 overflow-x-auto whitespace-nowrap">
@@ -94,7 +121,25 @@ export default function Page() {
           ))}
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4"></div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {hits.length > 0 ? (
+            hits.map((hit) => (
+              <div
+                key={hit.product_id}
+                className="aspect-square w-[80vw] md:w-[33vw] h-[400px] border"
+              >
+                <ProductCard
+                  payload={hit}
+                  sizes="(min-width: 768px) 33vw, 80vw"
+                />
+              </div>
+            ))
+          ) : (
+            <div className="my-4 w-full flex justify-center items-center md:col-span-3">
+              <NoSearchFound />
+            </div>
+          )}
+        </div>
       </div>
     </article>
   );
--- client-ui/lib/algolia.ts
+++ client-ui/lib/algolia.ts
@@ -26,4 +26,48 @@ export async function querySearch(query: string): Promise<IProductPayload[]> {
   return result;
 }
 
+export async function querySearchWithFilters({
+  query,
+  categories,
+  order,
+}: {
+  query: string;
+  categories: string[];
+  order: string[];
+}): Promise<IProductPayload[]> {
+  const { hits } = await algoliaIndex.search(query, {
+    filters: `category_name:${categories.join(" OR category_name:")}`,
+    facets: ["category_name"],
+    facetFilters: [`category_name:${categories.join(" OR category_name:")}`],
+    attributesToRetrieve: [
+      "product_id",
+      "product_name",
+      "product_desc",
+      "price",
+      "delivery_price",
+      "image_url",
+      "quantity",
+      "category_name",
+    ],
+    attributesToHighlight: ["product_name"],
+    attributesToSnippet: ["product_desc:10"],
+    hitsPerPage: 10,
+    page: 0,
+    order: order.join(","),
+  });
+
+  const result = hits.map((hit: any) => ({
+    product_id: hit.product_id,
+    product_name: hit.product_name,
+    product_desc: { String: hit.product_desc, Valid: true },
+    price: { Int32: hit.price, Valid: true },
+    delivery_price: { Int32: hit.delivery_price, Valid: true },
+    image_url: hit.image_url,
+    quantity: { Int32: hit.quantity, Valid: true },
+    category_name: hit.category_name,
+  }));
+
+  return result;
+}
+
 export default algoliaIndex;
